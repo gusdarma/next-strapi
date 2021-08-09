8f781b61eb0d2a70ee9e336e5c89b7bc93becb53
gusdarma <gusdarmap@gmail.com>
2021-08-09 11:30:31 +0800

    feat: add strapi, update react and nextjs

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8041190571176f6af1803df38c29e074'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '752d0794-14d8-4fdf-8cbf-4833fe2f1571'
+};
