33c086acb4dd4064de94144ae725fa8fad4b4f87
gusdarma <gusdarmap@gmail.com>
2021-09-22 11:29:19 +0800

    feat: add slugify and delete strapi-navigation-plugin

--- backend/api/page/models/page.js
+++ backend/api/page/models/page.js
@@ -1,8 +1,30 @@
-'use strict';
+"use strict";
 
 /**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const slugify = require("slugify");
+
+// Strips out special characters from the title to make it url-friendly
+
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      console.log(data, "ini datanya dimodel");
+      if (data.slug) {
+        data.slug = slugify(data.slug, { lower: true });
+      } else if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.slug) {
+        data.slug = slugify(data.slug, { lower: true });
+      } else if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+    },
+  },
+};
